4943d592ece0723c7bb18fd07c250b44a711645b
chromato99 <chromato99@gmail.com>
2023-04-20 11:37:24 +0900

    Chage to use post request

--- src/main/java/com/aform_chat/chatbotdemo/controller/ChatController.java
+++ src/main/java/com/aform_chat/chatbotdemo/controller/ChatController.java
@@ -1,9 +1,12 @@
 package com.aform_chat.chatbotdemo.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -29,11 +32,17 @@ public class ChatController {
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
         messagingTemplate.convertAndSend("/topic/public", chatMessage);
 
-        String url = chatbotServerUrl + chatMessage.getContent();
         RestTemplate restTemplate = new RestTemplate();
 
-        // Get Request
-        HttpEntity<String> response = restTemplate.getForEntity(url, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HashMap<String, String> body = new HashMap<String, String>();
+        body.put("msg", chatMessage.getContent());
+
+        HttpEntity<HashMap<String, String>> request = new HttpEntity<>(body, headers);
+        // Post Request
+        HttpEntity<String> response = restTemplate.postForEntity(chatbotServerUrl, request, String.class);
 
         ChatMessage responseMessage = ChatMessage.builder()
                 .type(ChatMessage.MessageType.CHAT)
